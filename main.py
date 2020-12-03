c7fddae5347a83dc5cb49877e998a650ba8e0a97
Iván <68256675+IvanUt4@users.noreply.github.com>
2020-12-02 22:36:48 -0700

    Comentarios
    Comentarios

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ import db_config as db
 
 app = create_app()
 
+# Token para poder hacer uso de ciertas funciones.
 key = "MissingNo151"
 
 
-
+# Ruta y función para poder obtener toda la información de la API.
 @app.route('/all_teams/', methods=['GET'])
 def show_teams():
     all_teams = dumps(list(db.db.MakeTeams.find()))
     return all_teams
 
-
+#Ruta y función para poder obtener información de un usuario en específico en base en su nombre de la API.
 @app.route(f'/MakeTeams/<string:n_key>/', methods=['GET'])
 def show_a_team(n_key):
     team = dumps(db.db.MakeTeams.find_one({'user_name':n_key}))
     return team
 
-
+# Ruta y función para agregar un equipo a la API con un nuevo JSON.
 @app.route(f'/{key}/new_team/', methods=['POST'])
 def add_new_team():
     
@@ -39,7 +40,7 @@ def add_new_team():
         "status": 200,
     })
 
-
+# Ruta y función para actualizar la información de un usuario en base a su nombre de la API con un nuevo JSON.
 @app.route(f'/{key}/MakeTeams/update/<string:n_key>',methods=['PUT'])
 def update_team(n_key):
 
@@ -59,6 +60,7 @@ def update_team(n_key):
 
     return jsonify({"status":200,f"el equipo{n_key} fue actualizado"})
 
+# Ruta y función para eliminar un usuario en base a su nombre de la API.
 
 @app.route(f'/{key}/MakeTeams/del/<string:n_key>',methods=['DELETE'])
 def delete_team(n_key):
@@ -68,6 +70,6 @@ def delete_team(n_key):
         return jsonify({"status":400,  f"equipo {n_key} no encontrado"})
     return jsonify({"status":200,f"el equipo {n_key} fue eliminado"})
 
-
+# # Verificación para saber si el archivo es el main, si es el caso se ejecutará app.run.
 if __name__ == '__main__':
     app.run(load_dotenv=True, port=8080)
